ac68ccd98a9e817d09002829f9d04511db32cf62
Max Vontz <fullstacksmax@gmail.com>
2025-02-22 13:59:28 -0600

    home and search created

--- assignment-app/src/App.jsx
+++ assignment-app/src/App.jsx
@@ -1,15 +1,54 @@
 import React from "react";
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from "./Components/Home";
+import Search from './Components/Search'
 
 function App() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         {/* <p> tag is a placeholder, You'll need to change the tag/component type later*/}
-        <p className="navbar-brand ms-4 nav-link">User Behavior Data</p>
+        <a className="navbar-brand ms-4 nav-link">User Behavior Data </a>
+        <Link className="navbar-brand ms-4 nav-link" to='home'>Home</Link>
+        <Link className="navbar-brand ms-4 nav-link" to='Search'>Search</Link>      
+        
+        
       </nav>
       <hr />
+      <Routes>
+       <Route path='home' element={<Home />} />
+       <Route path='Search' element={<Search />} />
+      </Routes>
+      
+      
     </div>
   );
 }
 
 export default App;
+
+
+/* import {Routes, Route, Link} from 'react'
+<Link classname="navbar-brand ms-4 nav-link" path='/' >Home</Link>
+<Link to='/' >Home </Link>
+<Routes>
+<Route path='/' element={<Home />} />
+</Routes> */
+
+
+
+/*
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        { <p> tag is a placeholder, You'll need to change the tag/component type later}
+        <p className="navbar-brand ms-4 nav-link">User Behavior Data <Link to='/Search' > Search</Link> </p>
+        
+        
+      </nav>
+      <hr />
+      
+    </div>
+  );
+}*/
---
+++ assignment-app/src/Components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+
+
+
+export default function Search() {
+    return (
+        <div>
+            search page
+        </div>
+    )
+}
