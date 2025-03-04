41ed4058efdc29982f45fc8f4da5f61c79c253c0
Max V <fullstacksmax@gmail.com>
2025-03-04 22:01:16 +0000

    median fixed.

--- assignment-app/src/Components/DataStats.jsx
+++ assignment-app/src/Components/DataStats.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react"
+import { useEffect, useState, useContext, useCallback } from "react"
 import SearchContextProvider, { SearchContext } from './Context'
 import Cards from "./Cards"
 
@@ -6,7 +6,7 @@ import Cards from "./Cards"
 
 const DataStats = function DataStats({ keyword }) {
     const { searchContext, dataContext, setDataContext } = useContext(SearchContext)
-    const [count, setCount] = useState("")
+    const [count, setCount] = useState(dataContext.length)
 
     const { avgAge, setAvgAge } = useContext(SearchContext)
     const { avgInstalledApps, setAvgInstalledApps } = useContext(SearchContext)
@@ -28,64 +28,69 @@ const DataStats = function DataStats({ keyword }) {
     if (!dataContext) {
         return null
     }
-    useEffect(() => {
+
+    const createArrays = useCallback(() => {
         const age = []
         const installedApps = []
         const screenTime = []
         const appUsage = []
+
+        dataContext.forEach(e => {
+
+            age.push(Number(e.Age))
+            installedApps.push(Number(e["Number of Apps Installed"]))
+            screenTime.push(Number(e["Screen On Time (hours/day)"]))
+            appUsage.push(Number(e["App Usage Time (min/day)"]))
+
+        })
+        age.sort()
+        installedApps.sort()
+        screenTime.sort()
+        appUsage.sort()
+        console.log(appUsage)
+        return({
+            //its not much fun when .sort() doesnt sort the way you think it does.
+            agePass : age.sort((a, b) => a - b),
+            installedAppsPass : installedApps.sort((a, b) => a - b),
+            screenTimePass: screenTime.sort((a, b) => a - b),
+            appUsagePass: appUsage.sort((a, b) => a - b)
+        })
+    }, [dataContext])
+
+    
+
+    useEffect(() => {
+        const { agePass, installedAppsPass,screenTimePass, appUsagePass } = createArrays();
+        //console.log(appUsagePass)
         setCount(dataContext.length)
 
-        const statsPage = () => {
-            setAvgAge((ageArr.reduce((acc, v) => acc + v, 0) / ageArr.length).toFixed((0)));
-            setAvgInstalledApps((installedAppsArr.reduce((acc, v) => acc + v, 0) / installedAppsArr.length).toFixed((0)))
-            setAvgScreenTime((screenTimeArr.reduce((acc, v) => acc + v, 0) / screenTimeArr.length).toFixed((0)));
-            setAvgAppUsage((appUsageArr.reduce((acc, v) => acc + v, 0) / appUsageArr.length).toFixed((0)));
+        
+            setAvgAge((agePass.reduce((acc, v) => acc + v, 0) / agePass.length).toFixed((0)));
+            setAvgInstalledApps((installedAppsPass.reduce((acc, v) => acc + v, 0) / installedAppsPass.length).toFixed((0)))
+            setAvgScreenTime((screenTimePass.reduce((acc, v) => acc + v, 0) / screenTimePass.length).toFixed((0)));
+            setAvgAppUsage((appUsagePass.reduce((acc, v) => acc + v, 0) / appUsagePass.length).toFixed((0)));
 
-            setMedAge(((ageArr[count / 2] + ageArr[(count / 2) + 1]) / 2).toFixed(1))
-            setMedInstalledApps(((installedAppsArr[count / 2] + installedAppsArr[(count / 2) + 1]) / 2).toFixed(1))
-            setMedScreenTime(((screenTimeArr[count / 2] + screenTimeArr[(count / 2) + 1]) / 2).toFixed(1))
-            setMedAppUsage(((appUsageArr[count / 2] + appUsageArr[(count / 2) + 1]) / 2).toFixed(1))
+            if(dataContext.length % 2 === 0) {
+            setMedAge(((agePass[count / 2] + agePass[(count / 2) + 1]) / 2).toFixed(1))
+            setMedInstalledApps(((installedAppsPass[count / 2] + installedAppsPass[(count / 2) + 1]) / 2).toFixed(1))
+            setMedScreenTime(((screenTimePass[count / 2] + screenTimePass[(count / 2) + 1]) / 2).toFixed(1))
+            setMedAppUsage(((appUsagePass[count / 2] + appUsagePass[(count / 2) + 1]) / 2).toFixed(1))
+        }
 
 
-            if (count % 2 != 0) {
+            if (dataContext.length % 2 != 0) {
 
-
-                setMedAge((ageArr[(count / 2) - 0.5]).toFixed(1))
-                setMedInstalledApps((installedAppsArr[(count / 2) - 0.5]).toFixed(1))
-                setMedScreenTime((screenTimeArr[(count / 2) - 0.5]).toFixed(1))
-                setMedAppUsage((appUsageArr[(count / 2) - 0.5]).toFixed(1))
+                console.log(appUsagePass)
+                console.log((dataContext.length / 2) - 0.5)
+                setMedAge((agePass[(dataContext.length / 2) - 0.5]).toFixed(1))
+                setMedInstalledApps((installedAppsPass[(dataContext.length / 2) - 0.5]).toFixed(1))
+                setMedScreenTime((screenTimePass[(dataContext.length / 2) - 0.5]).toFixed(1))
+                setMedAppUsage((appUsagePass[(dataContext.length / 2) - 0.5]).toFixed(1))
             }
-            if (dataContext != null) createArrays();
-        }
-        const createArrays = () => {
-            dataContext.forEach(e => {
+            
+        
 
-                age.push(Number(e.Age))
-                installedApps.push(Number(e["Number of Apps Installed"]))
-                screenTime.push(Number(e["Screen On Time (hours/day)"]))
-                appUsage.push(Number(e["App Usage Time (min/day)"]))
-
-            })
-            age.sort()
-            installedApps.sort()
-            screenTime.sort()
-            appUsage.sort()
-            setScreenTimeArr(screenTime)
-            setAppUsageArr(appUsage)
-            setInstalledAppsArr(installedApps)
-            setAgeArr(age)
-        }
-
-
-        createArrays();
-        statsPage();
-
-
-
-
-
-
-    }, [dataContext, count, keyword])
+    }, [dataContext, createArrays])
 
 
 
